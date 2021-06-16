0320ef7f1248ef603caed93f2db78cc368cdeb8f
Innf107 <finn@famschneider.info>
2021-06-16 14:20:22 +0200

    simplified test setup

--- test/runTests.sh
+++ test/runTests.sh
@@ -7,13 +7,13 @@ for f in $(find test -name "*.mcfunction" -type f)
 do
     expected=$(grep -Po '(?<=#EXPECT:).*' $f)
     result=$(out/andesite $f)
-    if [ -n "$(echo "$result" | tail -n 1 | grep -Po "$expected")" ] 
+    if [ $(echo "$result" | tail -n 1 | grep -Po "$expected") ] 
     then
         echo -e "\e[32mPassed: $f\e[0m"
     else
         echo -e "\e[31mFAILED!!!: $f"
         echo -e "    Expected: $expected"
-        echo -e "    Received: $result\e[0m"
+        echo -e "    Received: \n$result\e[0m"
     fi
 done;
 
